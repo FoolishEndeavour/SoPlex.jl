26bdaea677a09f731036b33abcf965368c7a1c90
JT <tidswell@maths.usyd.edu.au>
2024-04-10 16:03:58 +1000

    allow generating wrapper from SCIP_jll

--- gen/generate_wrapper.jl
+++ gen/generate_wrapper.jl
@@ -2,24 +2,38 @@ using Clang
 
 const HEADER_BASE = "/usr/include"
 
-const SOPLEX_SRC = ENV["SOPLEX_SRC_DIR"]
-@assert ispath(SOPLEX_SRC)
-
-const SOPLEX_BUILD_DIR = if haskey(ENV, "SOPLEX_BUILD_DIR")
-    ENV["SOPLEX_BUILD_DIR"]
-else
-    p = joinpath(SOPLEX_SRC, "build")
-    if ispath(p)
-        p
-    else
-        p = joinpath(SOPLEX_SRC, "debug")
-    end
-    p
+# a private API is risky, but Pkg package does not provide a quiet way
+const hasSCIP = (Base.find_package("SCIP_jll") !== nothing)
+if hasSCIP
+    import SCIP_jll
 end
 
-@assert ispath(SOPLEX_BUILD_DIR)
+src_dir = get(ENV, "SOPLEX_SRC_DIR", "")
+if length(src_dir) > 0 then
+    if !isdir(src_dir)
+        error("SOPLEX_SRC_DIR ($(src_dir)) is not an accessible directory.")
+    end
+    src_dir = joinpath(SOPLEX_SRC, "src")
+    if !isdir(src_dir)
+        error("\'src\' subdirectory of SOPLEX_SRC_DIR ($(src_dir)) is not an accessible directory.")
+    end
+end
 
-soplex_header = joinpath(SOPLEX_SRC, "src", "soplex_interface.h")
+if length(src_dir) == 0 
+    if !hasSCIP
+        error("Unable to locate SOPLEX_SRC_DIR.\nTry running \'import Pkg; Pkg.add(\"SCIP_jll\")\'.")
+    else
+        src_dir = joinpath(dirname(SCIP_jll.libsoplex), "..", "include")
+        if !isdir(src_dir)
+            error("Unable to locate source include files in SCIP_jll package.")
+        end
+    end
+end
+
+const SOPLEX_SRC = src_dir
+@assert ispath(SOPLEX_SRC)
+
+soplex_header = joinpath(SOPLEX_SRC, "soplex_interface.h")
 @assert isfile(soplex_header)
 
 context = Clang.init(
